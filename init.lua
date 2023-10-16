8131730b0700e43443ce7f1fc901617bc342d1b7
Simon Ward <39803787+simonward87@users.noreply.github.com>
2023-10-16 19:13:49 +0100

    Programmatically set python3_host_prog variable

--- init.lua
+++ init.lua
@@ -1,6 +1,11 @@
 vim.g["loaded_perl_provider"] = 0
-vim.g["python3_host_prog"] = "/opt/homebrew/bin/python3"
 vim.g["loaded_netrw"] = 1
 vim.g["loaded_netrwPlugin"] = 1
+
+if vim.loop.os_uname().sysname == "Darwin" then
+	vim.g["python3_host_prog"] = "/opt/homebrew/bin/python3"
+else
+	vim.g["python3_host_prog"] = "/usr/bin/python3"
+end
 
 require("simonward")
