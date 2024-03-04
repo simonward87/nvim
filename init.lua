8bfbdc83c03bc3850c3a3c8fb7e872ab833f68e1
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-03-04 23:01:39 +0000

    Remove Linux neovim providers

--- init.lua
+++ init.lua
@@ -8,11 +8,6 @@ if vim.loop.os_uname().sysname == "Darwin" then
 	vim.g["perl_host_prog"] = vim.fn.expand("$HOMEBREW_PREFIX/bin/perl")
 	vim.g["ruby_host_prog"] = "/usr/bin/ruby"
 	vim.g["node_host_prog"] = vim.fn.expand("$N_PREFIX/lib/node_modules/neovim")
-elseif vim.loop.os_uname().sysname == "Linux" then
-	vim.g["perl_host_prog"] = "/usr/bin/perl"
-	vim.g["python3_host_prog"] = "/usr/bin/python3"
-	vim.g["ruby_host_prog"] = "/usr/bin/ruby"
-	vim.g["node_host_prog"] =  "/usr/local/lib/node_modules/neovim"
 end
 
 require("simonward")
