b38744833413513625b72453aa1d743a7a9dd973
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-03-04 21:38:41 +0000

    Setup provider paths

--- init.lua
+++ init.lua
@@ -3,8 +3,11 @@ vim.g["loaded_netrwPlugin"] = 1
 vim.g["skip_ts_context_commentstring_module"] = true
 
 if vim.loop.os_uname().sysname == "Darwin" then
-	vim.g["loaded_python3_provider"] = 0
-	-- vim.g["python3_host_prog"] = "/opt/homebrew/bin/python3"
+	vim.g["loaded_python3_provider"] = 0 -- disabled as not functional
+
+	vim.g["perl_host_prog"] = vim.fn.expand("$HOMEBREW_PREFIX/bin/perl")
+	vim.g["ruby_host_prog"] = "/usr/bin/ruby"
+	vim.g["node_host_prog"] = vim.fn.expand("$N_PREFIX/lib/node_modules/neovim")
 else
 	vim.g["python3_host_prog"] = "/usr/bin/python3"
 end
