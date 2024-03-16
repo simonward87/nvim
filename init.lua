8f46fa5ad7814ddaa4898b0a4ab07869a0d41850
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-03-16 20:01:00 +0000

    Enable python provider within venv

--- init.lua
+++ init.lua
@@ -3,9 +3,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.skip_ts_context_commentstring_module = true
 
 if vim.loop.os_uname().sysname == "Darwin" then
-	vim.g.loaded_python3_provider = 0 -- disabled as not functional
-	-- vim.g.python3_host_prog = vim.fn.expand("$HOMEBREW_PREFIX/bin/python3")
-
+	vim.g.python3_host_prog = vim.fn.expand("~/.local/venvs/neovim/bin/python")
 	vim.g.perl_host_prog = vim.fn.expand("$HOMEBREW_PREFIX/bin/perl")
 	vim.g.ruby_host_prog = "/usr/bin/ruby"
 	vim.g.node_host_prog = vim.fn.expand("$N_PREFIX/lib/node_modules/neovim")
