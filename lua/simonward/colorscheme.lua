be60e2eda7de35d2f645ff69a5c4651540e317c0
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-03-20 21:16:24 +0000

    Cleanup unused themes, refactor

--- lua/simonward/colorscheme.lua
+++ lua/simonward/colorscheme.lua
@@ -1,10 +1,3 @@
 local c = "drift-light"
-local custom_config = pcall(require, "simonward.colors." .. c)
 
-if not custom_config then
-	vim.cmd("colorscheme " .. c)
-end
-
-if c == "gruvbox_material" then
-	vim.g.gruvbox_material_palette = "mix"
-end
+vim.cmd("colorscheme " .. c)
--- lua/simonward/plugins.lua
+++ lua/simonward/plugins.lua
@@ -43,75 +43,65 @@ packer.init({
 })
 
 return packer.startup(function(use)
-	use("akinsho/bufferline.nvim") -- bufferline along top of window
-	use("JoosepAlviste/nvim-ts-context-commentstring") -- simple code commenting
-	use({ "nvim-tree/nvim-tree.lua", requires = { "nvim-tree/nvim-web-devicons" } }) -- file explorer
-	use("lewis6991/gitsigns.nvim") -- git info in sign column
-	use("lukas-reineke/indent-blankline.nvim") -- add guide rules to indentation
-	use("mfussenegger/nvim-dap") -- debug adapter
-	use("norcalli/nvim-colorizer.lua") -- hex code colourizer
-	use("nvim-lua/plenary.nvim") -- lua function library (dependency for other plugs)
-	use("nvim-lua/popup.nvim") -- An implementation of vim Popup API
-	use("nvim-telescope/telescope.nvim") -- fuzzy finder
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use("nvim-treesitter/nvim-treesitter-context") -- pin code context (current block)
-	use("nvim-treesitter/playground") -- view treesitter info
-	use("tpope/vim-capslock") -- software capslock
-	use("tpope/vim-commentary") -- comment stuff out
-	use("tpope/vim-fugitive") -- git wrapper
-	use("tpope/vim-surround") -- streamline surroundings workflow
-	use("wbthomason/packer.nvim") -- Let packer manage itself
-	use("windwp/nvim-ts-autotag") -- auto-close & auto-rename html tags
-	use("windwp/nvim-autopairs") -- autopairs with cmp and treesitter integration
-
-	-- Colorschemes
-	use("axvr/photon.vim")
-	use({ "catppuccin/nvim", as = "catppuccin" })
-	use("cocopon/iceberg.vim")
-	use("comfysage/twilight-moon")
-	use("gosukiwi/vim-atom-dark")
-	use("habamax/vim-habaurora")
-	use("jacoborus/tender.vim")
-	use("nelstrom/vim-mac-classic-theme")
-	use("olivercederborg/poimandres.nvim")
-	use("projekt0n/github-nvim-theme")
-	use("rebelot/kanagawa.nvim")
-	use("sainnhe/gruvbox-material")
-
-	-- Completion
-	use("hrsh7th/cmp-buffer") -- buffer completions
-	use("hrsh7th/cmp-cmdline") -- cmdline completions
-	use("hrsh7th/cmp-nvim-lsp") -- lsp completions
-	use("hrsh7th/cmp-nvim-lua") -- complete neovim lua
-	use("hrsh7th/cmp-path") -- path completions
-	use("hrsh7th/nvim-cmp") -- completion plugin
-	use("L3MON4D3/LuaSnip") -- snippet engine
-	use("rafamadriz/friendly-snippets") -- snippet library
-	use("saadparwaiz1/cmp_luasnip") -- snippet completions
-
-	-- LSP
 	if vim.loop.os_uname().sysname == "Darwin" then
-		use("~/Work/projects/plugins/nvim/drift.nvim") -- simonward87/drift.nvim
+		use("~/Work/projects/plugins/nvim/drift.nvim")
 		use("~/Work/projects/plugins/nvim/nameless.nvim")
 		use("~/Work/projects/plugins/nvim/monoLemon.nvim")
 
-		use("b0o/SchemaStore.nvim") -- schemastore catalog access
+		use("b0o/SchemaStore.nvim") -- Schemastore catalog access
 		use({
-			"jay-babu/mason-null-ls.nvim", -- for formatters and linters
+			"jay-babu/mason-null-ls.nvim", -- Formatters and linters
 			requires = {
 				"williamboman/mason.nvim",
 				"nvimtools/none-ls.nvim",
 			},
 		})
-		use("neovim/nvim-lspconfig") -- enable LSP
-		use("sago35/tinygo.vim") -- tinygo extension
-		use("tamago324/nlsp-settings.nvim") -- language server settings defined in json for
-		use("williamboman/mason.nvim") -- package manager
-		use("williamboman/mason-lspconfig.nvim") -- package manager
-	elseif vim.loop.os_uname().sysname == "Linux" then
+		use("neovim/nvim-lspconfig") -- Enable LSP
+		use("sago35/tinygo.vim") -- Tinygo extension
+		use("tamago324/nlsp-settings.nvim") -- Json lsp config parser
+		use("williamboman/mason.nvim") -- Package manager
+		use("williamboman/mason-lspconfig.nvim") -- Package manager
+	else
 		use("simonward87/drift.nvim")
 	end
 
+	use("JoosepAlviste/nvim-ts-context-commentstring") -- Code commenting
+	use("L3MON4D3/LuaSnip") -- Snippet engine
+	use("akinsho/bufferline.nvim") -- List buffers along top
+	use("hrsh7th/cmp-buffer") -- Buffer completions
+	use("hrsh7th/cmp-cmdline") -- Cmdline completions
+	use("hrsh7th/cmp-nvim-lsp") -- LSP completions
+	use("hrsh7th/cmp-nvim-lua") -- Neovim lua completions
+	use("hrsh7th/cmp-path") -- Path completions
+	use("hrsh7th/nvim-cmp") -- Completion
+	use("lewis6991/gitsigns.nvim") -- Sign column git info
+	use("lukas-reineke/indent-blankline.nvim") -- Indentation guide lines
+	use("mfussenegger/nvim-dap") -- Debug adapter
+	use("norcalli/nvim-colorizer.lua") -- Hex code colourizer
+	use("nvim-lua/popup.nvim") -- Popup API
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { "nvim-lua/plenary.nvim" },
+	}) -- Fuzzy finder
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = { "nvim-tree/nvim-web-devicons" },
+	}) -- File explorer
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use("nvim-treesitter/nvim-treesitter-context") -- Pin code context (current block)
+	use("nvim-treesitter/playground") -- View treesitter info
+	use("rafamadriz/friendly-snippets") -- Snippet library
+	use("saadparwaiz1/cmp_luasnip") -- Snippet completions
+	use("tpope/vim-capslock") -- Software capslock
+	use("tpope/vim-commentary") -- Comment stuff out
+	use("tpope/vim-fugitive") -- Git wrapper
+	use("tpope/vim-surround") -- Streamline surroundings workflow
+	use("wbthomason/packer.nvim") -- Let packer manage itself
+	use("windwp/nvim-autopairs") -- Autopairs with cmp and treesitter integration
+	use("windwp/nvim-ts-autotag") -- Auto-close & auto-rename html tags
 
 	-- Automatically set up configuration after cloning packer
 	-- NOTE: must run after all plugins
