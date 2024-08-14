6bcd37dcd6f8c5e8e08dd1adb2dfbf80976c5a74
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-08-14 17:41:24 +0100

    Update deprecated API calls

--- init.lua
+++ init.lua
@@ -3,11 +3,11 @@ vim.g.loaded_netrwPlugin = 1
 vim.g.skip_ts_context_commentstring_module = true
 vim.g.ruby_host_prog = "/usr/bin/ruby"
 
-if vim.loop.os_uname().sysname == "Darwin" then
+if vim.uv.os_uname().sysname == "Darwin" then
 	vim.g.node_host_prog = vim.fn.expand("$N_PREFIX/lib/node_modules/neovim")
 	vim.g.perl_host_prog = vim.fn.expand("$HOMEBREW_PREFIX/bin/perl")
 	vim.g.python3_host_prog = vim.fn.expand("~/.local/venvs/neovim/bin/python")
-elseif vim.loop.os_uname().sysname == "Linux" then
+elseif vim.uv.os_uname().sysname == "Linux" then
 	vim.g.node_host_prog = "/usr/local/lib/node_modules/neovim/bin/cli.js"
 	vim.g.perl_host_prog = "/usr/bin/perl"
 	vim.g.python3_host_prog = vim.fn.expand("~/.local/venvs/neovim/bin/python")
