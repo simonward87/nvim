11df33fce1fe75cb5645df455302407f9323bbd8
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-09-27 13:52:08 +0100

    Update k8s schema reference

--- lua/simonward/lsp/settings/yamlls.lua
+++ lua/simonward/lsp/settings/yamlls.lua
@@ -3,7 +3,7 @@ return {
 		yaml = {
 			schemas = {
 				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-				["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] = "/*.k8s.yaml",
+				["https://kubernetesjsonschema.dev/v1.18.1-standalone-strict/all.json"] = "*",
 				["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
 				["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] = "skaffold.yaml",
 			},
