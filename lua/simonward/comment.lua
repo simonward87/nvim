7741116e87791d61b24ea0cab47e8374da453078
Simon Ward <39803787+simonward87@users.noreply.github.com>
2024-11-20 10:53:42 +0000

    Use native commenting

--- lua/simonward/comment.lua
+++ lua/simonward/comment.lua
@@ -6,3 +6,11 @@ end
 comment.setup({
 	enable_autocmd = false,
 })
+
+-- https://github.com/JoosepAlviste/nvim-ts-context-commentstring/wiki/Integrations#native-commenting-in-neovim-010
+local get_option = vim.filetype.get_option
+
+vim.filetype.get_option = function(filetype, option)
+	return option == "commentstring" and require("ts_context_commentstring.internal").calculate_commentstring()
+		or get_option(filetype, option)
+end
