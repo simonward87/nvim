e18739cfc2ae9daca73538e7757570b4106a5a36
Simon Ward <39803787+simonward87@users.noreply.github.com>
2025-05-11 20:50:17 +0100

    Update filetype definitions

--- lua/simonward/filetypes.lua
+++ lua/simonward/filetypes.lua
@@ -1,19 +1,21 @@
 -- Maps custom / niche extensions to their underlying filetype
 vim.filetype.add({
 	extension = {
+		envrc = "sh",
 		gohtml = "html",
+		sqlfluff = "toml",
+		sqliterc = "sh",
 		templ = "templ",
 		tmpl = "html",
 	},
 	filename = {
-		[".envrc"] = "sh",
-		[".sqlfluff"] = "toml",
 		["Caddyfile"] = "caddyfile",
 		["gitconfig"] = "gitconfig",
-		["go.mod"] = "gomod",
+		["sqliterc"] = "sh",
 		["zprofile"] = "zsh",
 	},
 	pattern = {
+		[".env.*"] = "sh",
 		["Dockerfile.*"] = "dockerfile",
 	},
 })
