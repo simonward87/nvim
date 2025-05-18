ae53ea85b478eba8e26efcb19bd7a2f58715a7a7
Simon Ward <39803787+simonward87@users.noreply.github.com>
2025-05-18 12:58:19 +0100

    Set default theme

--- lua/custom/lsp/settings/yamlls.lua
+++ lua/custom/lsp/settings/yamlls.lua
@@ -2,10 +2,10 @@ return {
 	settings = {
 		yaml = {
 			schemas = {
-				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-				["https://kubernetesjsonschema.dev/v1.18.1-standalone-strict/all.json"] = "*",
 				["http://json.schemastore.org/kustomization"] = "kustomization.yaml",
+				["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
 				["https://raw.githubusercontent.com/GoogleContainerTools/skaffold/master/docs/content/en/schemas/v2beta8.json"] = "skaffold.yaml",
+				["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json"] = "/*.k8s.yaml",
 			},
 		},
 	},
