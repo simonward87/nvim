8f1437044a6820b64456be66e6e1340f483be0ee
Simon Ward <39803787+simonward87@users.noreply.github.com>
2025-05-26 11:48:44 +0100

    Add ivy telescope theme to defaults

--- plugin/telescope.lua
+++ plugin/telescope.lua
@@ -1,39 +1,18 @@
-local telescope_ok, telescope = pcall(require, "telescope")
-if not telescope_ok then
-	return
-end
-
 local actions = require("telescope.actions")
 local builtin = require("telescope.builtin")
-local themes = require("telescope.themes")
 
 local function map(mode, lhs, rhs, opts)
 	local defaults = { noremap = true, silent = true }
 	vim.keymap.set(mode, lhs, rhs, opts or defaults)
 end
 
--- Themes: https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#themes
-map("n", "<leader>f", function()
-	builtin.find_files(themes.get_ivy())
-end)
+map("n", "<leader>f", builtin.find_files)
+map("n", "<leader>h", builtin.help_tags)
+map("n", "<leader>m", builtin.keymaps)
+map("n", "<C-t>", builtin.live_grep)
 
-map("n", "<leader>h", function()
-	builtin.help_tags(themes.get_ivy())
-end)
-
-map("n", "<leader>m", function()
-	builtin.keymaps(themes.get_ivy())
-end)
-
-map("n", "<c-t>", function()
-	builtin.live_grep(themes.get_ivy())
-end)
-
-telescope.setup({
+local opts = {
 	defaults = {
-		prompt_prefix = " ",
-		selection_caret = "  ",
-		path_display = { "smart" },
 		mappings = {
 			i = {
 				["<esc>"] = actions.close,
@@ -41,5 +20,23 @@ telescope.setup({
 				["<C-y>"] = actions.file_edit,
 			},
 		},
+		path_display = { "smart" },
+		prompt_prefix = " ",
+		selection_caret = "  ",
 	},
-})
+}
+
+-- Apply ivy theme to defaults
+-- https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#themes
+local get_ivy_ok, theme_opts = pcall(require("telescope.themes").get_ivy, opts)
+if get_ivy_ok then
+	opts.defaults = vim.tbl_deep_extend("force", theme_opts, opts)
+end
+
+local telescope_ok, telescope = pcall(require, "telescope")
+if not telescope_ok then
+	print("Error loading plugin: telescope")
+	return
+end
+
+telescope.setup(opts)
